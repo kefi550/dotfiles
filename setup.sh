91830fd829fd9964e8c68955452cb635364dd2f8
kefi550 <ytiryrstd550@gmail.com>
2019-12-18 00:25:03 +0900

    tmux.conf追加

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ ln -snfv $DOTFILE_DIR/_zprofile ~/.zprofile
 ln -snfv $DOTFILE_DIR/_vimrc ~/.vimrc
 #ln -snfv $DOTFILE_DIR./_gitconfig ~/.gitconfig
 ln -snfv $DOTFILE_DIR/_config ~/.config
+ln -snfv $DOTFILE_DIR/_tmux.conf ~/.tmux.conf

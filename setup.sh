acd4a479b842a728990635edb600e4870a14676d
kefi550 <ytiryrstd550@gmail.com>
2019-12-17 23:24:20 +0900

    zsh zpluginに変更とか

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 ln -snfv ~/dotfiles/_zshrc ~/.zshrc
+ln -snfv ~/dotfiles/zfiles/.z* ~/.zfiles
 ln -snfv ~/dotfiles/_zprofile ~/.zprofile
 ln -snfv ~/dotfiles/_vimrc ~/.vimrc
-ln -snfv ~/dotfiles/_gitconfig ~/.gitconfig
+#ln -snfv ~/dotfiles/_gitconfig ~/.gitconfig
 ln -snfv ~/dotfiles/_config ~/.config

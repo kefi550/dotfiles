651328d1acccaac38eaf578f6154c903edbdbddf
石橋 翔太 <shota.ishibashi@e-seikatsu.co.jp>
2019-12-19 14:14:42 +0900

    zplugin

--- setup.sh
+++ setup.sh
@@ -9,3 +9,5 @@ ln -snfv $DOTFILE_DIR/_vimrc ~/.vimrc
 #ln -snfv $DOTFILE_DIR./_gitconfig ~/.gitconfig
 ln -snfv $DOTFILE_DIR/_config ~/.config
 ln -snfv $DOTFILE_DIR/_tmux.conf ~/.tmux.conf
+
+# TODO: zpluginのinstall

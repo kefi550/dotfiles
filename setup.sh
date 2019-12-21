63323b21f0338fa6b301bb97da6b61ef8388a59e
kefi550 <ytiryrstd550@gmail.com>
2019-12-22 03:13:23 +0900

    setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ DOTFILE_DIR=$(pwd)
 
 ln -snfv $DOTFILE_DIR/_zshrc ~/.zshrc
 ln -snfv $DOTFILE_DIR/zfiles/.z* ~/.zfiles
+ln -snfv $DOTFILE_DIR/zfiles/* ~/.zfiles
 ln -snfv $DOTFILE_DIR/_zprofile ~/.zprofile
 ln -snfv $DOTFILE_DIR/_vimrc ~/.vimrc
 #ln -snfv $DOTFILE_DIR./_gitconfig ~/.gitconfig

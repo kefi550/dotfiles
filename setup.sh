5001323a769249a47e72f158b43727269f8c458f
kefi550 <ytiryrstd550@gmail.com>
2020-04-26 01:01:36 +0900

    zsh整理

--- setup.sh
+++ setup.sh
@@ -3,12 +3,10 @@
 DOTFILE_DIR=$(pwd)
 
 ln -snfv $DOTFILE_DIR/_zshrc ~/.zshrc
-ln -snfv $DOTFILE_DIR/zfiles/.z* ~/.zfiles
-ln -snfv $DOTFILE_DIR/zfiles/* ~/.zfiles
-ln -snfv $DOTFILE_DIR/_zprofile ~/.zprofile
+
+ln -snfv $DOTFILE_DIR/_config ~/.config
 ln -snfv $DOTFILE_DIR/_vimrc ~/.vimrc
 #ln -snfv $DOTFILE_DIR./_gitconfig ~/.gitconfig
-ln -snfv $DOTFILE_DIR/_config ~/.config
 ln -snfv $DOTFILE_DIR/_tmux.conf ~/.tmux.conf
 
 # TODO: zpluginのinstall

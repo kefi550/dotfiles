ed24b86efb6c2e50b7c0a3e7fbc4657e34b800a7
kefi550 <ytiryrstd550@gmail.com>
2021-03-15 15:05:14 +0900

    poetry

--- scripts/poetry.sh
+++ scripts/poetry.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/zsh
 
+sudo apt install python3-distutils
 which poetry &> /dev/null
 if [ $? != 0 ]; then
   curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
